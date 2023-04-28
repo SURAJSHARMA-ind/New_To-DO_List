147e06c5fa499ef4af29e3b85ab3642d420d354d
Suraj Sharma <80053118+SURAJSHARMA-ind@users.noreply.github.com>
2023-04-28 17:48:07 +0530

    change key name
    change the local storage key name because it is similar to previous project key name giving error

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const button = document.getElementById("btn");
 const text_container = document.querySelector(".text_container");
-// const text = document.getElementById("add_text");
 
 button.addEventListener("click", function() {
 
@@ -13,16 +12,24 @@ button.addEventListener("click", function() {
     let crsbtn = document.createElement("span");
     crsbtn.innerHTML = "X";
     text_container.appendChild(crsbtn);
+    // Add Save button
+    let savebtn = document.createElement("span");
+    savebtn.innerHTML = "&#10003";
+    savebtn.classList.add("savebtn")
+    text_container.appendChild(savebtn);
 
     // Attach event listener to new li element
     crsbtn.addEventListener("click", function(a) {
         text.remove();
         crsbtn.remove();
-        console.log("clicked")
+        savebtn.remove();
+        console.log("clicked");
       savedata();
     });
+   
     savedata();
 });
+
 
 // Load saved data on page load
 window.onload = function() {
@@ -30,25 +37,25 @@ window.onload = function() {
 };
 
 // Save data to local storage
-function savedata() {
-  localStorage.setItem("listItems", text_container.innerHTML);
+function savedata() { 
+  localStorage.setItem("note_list", text_container.innerHTML);
 }
 
-// Load data from local storage
-function showdata() {
-  if (localStorage.getItem("listItems")) {
-    text_container.innerHTML = localStorage.getItem("listItems");
+// // Load data from local storage
+// function showdata() {
+//   if (localStorage.getItem("note_list")) {
+//     text_container.innerHTML = localStorage.getItem("note_list");
 
-    // Attach event listeners to new textarea and span elements
-    const textareas = document.querySelectorAll(".text_container textarea");
-    const spans = document.querySelectorAll(".text_container span");
-    textareas.forEach(function(textarea, index) {
-      spans[index].addEventListener("click", function() {
-        textarea.remove();
-        spans[index].remove();
-        savedata();
-      });
-    });
-  }
-}
+//     // Attach event listeners to new textarea and span elements
+//     const textareas = document.querySelectorAll(".text_container textarea");
+//     const spans = document.querySelectorAll(".text_container span");
+//     textareas.forEach(function(textarea, index) {
+//       spans[index].addEventListener("click", function() {
+//         textarea.remove();
+//         spans[index].remove();
+//         savedata();
+//       });
+//     });
+//   }
+// }
 
